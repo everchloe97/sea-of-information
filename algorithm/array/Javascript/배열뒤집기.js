bc977542f11fad77f35368e6f42c709cd58eb1a6
everchloe <yeomhaneul@naver.com>
2023-10-10 12:08:20 +0900

    Add / array algorithm

---
+++ algorithm/array/Javascript/배열뒤집기.js
@@ -0,0 +1,7 @@
+const numlist = [1, 2, 3, 4, 5];
+
+const solution = (numlist) => numlist.reverse();
+
+console.log(solution(numlist));
+
+/**원리 풀이 */
---
+++ algorithm/array/Javascript/배열원소의길이.js
@@ -0,0 +1,4 @@
+const strlist = ["We", "are", "the", "world!"];
+
+const solution = (strlist) => strlist.map((el) => el.length);
+console.log(solution(strlist));
