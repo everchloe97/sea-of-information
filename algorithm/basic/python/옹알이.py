7c3a8009ad3bf09de3bcf5342f641a7318a4bbc1
haneul-iris27 <iris27@haneul-iris27s-MacBook-Pro.local>
2023-11-19 09:11:57 +0900

    Add / 순열, 조합

---
+++ algorithm/basic/python/옹알이.py
@@ -0,0 +1,10 @@
+babbling = ["aya", "yee", "u", "maa", "wyeoo"]
+
+from itertools import permutations
+
+def solution (babbling):
+    answer = 0
+    speek = ["aya", "yee", "woo", "ma"]
+    word = []
+    
+    
